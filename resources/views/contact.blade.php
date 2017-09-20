a4294e40629d7f395ae295655268e3f5329b8fad
Sander Pool <poolsander97@gmail.com>
2017-09-20 11:29:50 +0200

    added contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,9 +2,63 @@
 <html lang="en">
 <head>
     <title>Contact</title>
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
 <body>
-<section>
+<section class="content full-height">
     <h1>Contact information</h1>
     <h2>Visitors address</h2>
     <p>
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,8 +1,32 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Chofl</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Style -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+</head>
+</html>
 <body>
-    <h1>Create new user</h1>
+    <div class="create-form full-height content">
+        <h1>Create new user</h1>
         {!! Form::open(['url' => 'users']) !!}
-        {!! Form::text('firstname', 'voornaam') !!}
-        <span>{{ $errors ->first('firstname') }}</span>
+        {!! Form::text('username', ['placeholder' => 'Username']) !!}
+        <span>{{ $errors ->first('username') }}</span>
+        {!! Form::password('password', 'password') !!}
+        <span>{{ $errors ->first('password') }}</span>
+        {!! Form::email('email', 'email') !!}
+        <span>{{ $errors ->first('email') }}</span>
         {!! Form::submit() !!}
         {!! Form::close() !!}
+    </div>
 </body>
